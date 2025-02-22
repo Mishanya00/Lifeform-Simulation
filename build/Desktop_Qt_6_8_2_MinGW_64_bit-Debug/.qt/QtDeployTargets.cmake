1734204228b6eed5b40c02f34c123deebc88b3cf
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-02-22 18:59:15 +0300

    Revert "Base hierarchy of lifeform classes"
    This reverts commit 56ef508408dca45ebead15dfc4d1aaca8d568a57.

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LifeForm_Simulation_FILE D:/Education/BSUIR/Disciplines/OOP/LifeForm_Simulation/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/LifeForm_Simulation.exe)
-set(__QT_DEPLOY_TARGET_LifeForm_Simulation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LifeForm_Simulation_RUNTIME_DLLS D:/Programs/ToolsPROF/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;D:/Programs/ToolsPROF/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;D:/Programs/ToolsPROF/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
